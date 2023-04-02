66c40c05c1714d9d9a3815b232291136df565c36
Player788 <110638503+Player788@users.noreply.github.com>
2023-04-02 06:04:31 +0500

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,6 +1,6 @@
 local Library = {}
 Library.__index = Library
-_G.ESPVERSION = "1I"
+_G.ESPVERSION = "1j"
 setclipboard(_G.ESPVERSION)
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
